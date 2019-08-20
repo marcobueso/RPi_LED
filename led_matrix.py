0e6b39b4c1b6c89eb98d911b414ac9b2b36f6d48
marcobueso <mbueso97@gmail.com>
2019-08-19 18:29:46 -0700

    Create led_matrix.py
    Created led_matrix.py file. Added initial code

---
+++ led_matrix.py
@@ -0,0 +1,15 @@
+import importlib.util
+try:
+    importlib.util.find_spec('RPi.GPIO')
+    import RPi.GPIO as GPIO
+except ImportError:
+    """
+    import FakeRPi.GPIO as GPIO
+    OR
+    import FakeRPi.RPiO as RPiO
+    """
+	
+    import FakeRPi.GPIO as GPIO
+	
+
+    
